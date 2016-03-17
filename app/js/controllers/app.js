c9068e9e5b63b9f42b9113a0b8fa54fd3a95adb6
i.labetskaya <Irina.Labetskaya@ocsico.com>
2016-03-17 13:33:56 +0300

    arr without list

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -2,14 +2,14 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller("purchaseController", function ($scope) {
 
-    $scope.list = model;
+    $scope.items = model;
     $scope.addItem = function (firstName, wage, itemOrg ) {
         wage = parseFloat(wage); // преобразуем введенное значение к числу
         if(firstName != "" && itemOrg != "" && !isNaN(wage)) // если текст установлен и введено число, то добавляем
         {
-            $scope.list.items.push({ pushName: firstName, pushAge: wage, pushOrg: itemOrg});
+            $scope.items.push({ pushName: firstName, pushAge: wage, pushOrg: itemOrg});
 
-            var obgJsonNew = JSON.stringify( model );
+            var obgJsonNew = JSON.stringify(model);
             localStorage.setItem("newLS", obgJsonNew);
             console.log(localStorage);
         }
@@ -19,36 +19,26 @@ myApp.controller("purchaseController", function ($scope) {
         else{
             alert('введите число в поле возраст');
         }
-
     };
 
 });
-//
-//var model = [
-//    {
-//        pushName: "Илья",
-//        pushAge: 1,
-//        pushOrg: "First"
-//    },
-//    {
-//        pushName: "Ron",
-//        pushAge: 15,
-//        pushOrg: "Second"
-//    },
-//    {
-//        pushName: "Michail",
-//        pushAge: 2,
-//        pushOrg: "Last"
-//    }
-//];
 
-var model = {
-    items: [
-        { pushName: "Ron", pushAge: 28 ,pushOrg: "Second"},
-        { pushName: "Илья", pushAge: 22 ,pushOrg: "First"},
-        { pushName: "Michail", pushAge: 25 ,pushOrg: "Last"}
-    ]
-};
+var model =  [
+        {
+            pushName: "Ron",
+            pushAge: 28,
+            pushOrg: "Second"
+        },
+        {
+            pushName: "Илья",
+            pushAge: 22,
+            pushOrg: "First"
+        },
+        { pushName: "Michail",
+            pushAge: 25,
+            pushOrg: "Last"
+        }
+    ];
 
 var obgJson = JSON.stringify( model );
 localStorage.setItem("newLS", obgJson);
--- app/js/controllers/filter.js
+++ app/js/controllers/filter.js
@@ -9,33 +9,20 @@ $(function () {
     function addSort() {
         var retrievedData = localStorage.getItem("newLS");
         var storage = JSON.parse(retrievedData);
-        var myArray = storage.items;
-        console.log(myArray);
+//        var myArray = storage.items;
+        console.log(storage);
 
 //        var ageArray = [];
-        for (i = 0; i < myArray.length; i++) {
-//            alert(myArray[i].pushAge);
+//        for (i = 0; i < storage.length; i++) {
 //            ageArray.push(myArray[i].pushAge);
-        }
-        console.log(myArray.sort(compareNumeric));
-//        console.log(ageArray.sort(compareNumeric));
+//        }
+        console.log(storage.sort(compareNumeric));
     }
 
 
     $('#elemAge').on('click', function () {
         addSort();
     });
-
-//    $("#sort").click(function () {
-//        var mylist = $('#menu');
-//        var listitems = mylist.children('div').get();
-//        listitems.sort(function(a, b) {
-//            var compA = $(a).text().toUpperCase();
-//            var compB = $(b).text().toUpperCase();
-//            return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-//        })
-//        $.each(listitems, function(idx, itm) { mylist.append(itm); });
-//    });
 
 });
 
