7be5fb0c7113b5cd1ec17a198f665672508bcccf
i.labetskaya <Irina.Labetskaya@ocsico.com>
2016-03-18 10:51:44 +0300

    custom input

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -5,14 +5,26 @@ myApp.controller("purchaseController", function ($scope) {
     $scope.sortType     = 'pushAge'; // значение сортировки по умолчанию
     $scope.sortReverse  = false;  // обратная сортировка
 
-    $scope.items = model;
-    $scope.addItem = function (firstName, wage, itemOrg ) {
+
+
+
+    $scope.halue =  [];
+
+    $scope.addField = function(){
+        $scope.halue.push('');
+        console.log($scope.halue);
+    };
+
+console.log($scope.halue);
+    $scope.items = boxs;
+    $scope.addItem = function (firstName, wage, itemOrg, halue ) {
+
         wage = parseFloat(wage); // преобразуем введенное значение к числу
         if(firstName != "" && itemOrg != "" && !isNaN(wage)) // если текст установлен и введено число, то добавляем
         {
-            $scope.items.push({ pushName: firstName, pushAge: wage, pushOrg: itemOrg});
+            $scope.items.push({ pushName: firstName, pushAge: wage, pushOrg: itemOrg, pushNote: halue});
 
-            var obgJsonNew = JSON.stringify(model);
+            var obgJsonNew = JSON.stringify(boxs);
             localStorage.setItem("newLS", obgJsonNew);
             console.log(localStorage);
         }
@@ -24,26 +36,31 @@ myApp.controller("purchaseController", function ($scope) {
         }
     };
 
+
+
 });
 
-var model =  [
+var boxs =  [
         {
             pushName: "Ron",
             pushAge: 28,
-            pushOrg: "Second"
+            pushOrg: "Second",
+            pushNote: ""
         },
         {
             pushName: "Илья",
             pushAge: 22,
-            pushOrg: "First"
+            pushOrg: "First",
+            pushNote: ""
         },
         { pushName: "Michail",
             pushAge: 25,
-            pushOrg: "Last"
+            pushOrg: "Last",
+            pushNote: ""
         }
     ];
 
-var obgJson = JSON.stringify( model );
+var obgJson = JSON.stringify( boxs );
 localStorage.setItem("newLS", obgJson);
 
 
