2e8c0028507b1d8f2c0eb4c3a54ee0e1b5c414f1
i.labetskaya <Irina.Labetskaya@ocsico.com>
2016-03-18 17:52:44 +0300

    all

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -4,7 +4,8 @@ myApp.controller("purchaseController", function ($scope) {
     $scope.items = boxs;//добавление массива
     $scope.sortType     = 'pushAge'; // значение сортировки по умолчанию
     $scope.sortReverse  = false;  // обратная сортировка
-    $scope.addField = function(){
+    $scope.addField = function(note){
+//        $scope.halue = [];
         $scope.halue.push('');
         console.log($scope.halue);
     };
@@ -12,6 +13,8 @@ myApp.controller("purchaseController", function ($scope) {
         wage = parseFloat(wage); // преобразуем введенное значение к числу
         if(firstName != "" && itemOrg != "" && !isNaN(wage)){ // если текст установлен и введено число, то добавляем
             $scope.items.push({pushData: firstData, pushName: firstName, pushTime: firstTime, pushAge: wage, pushOrg: itemOrg, pushNote: halue});
+            $scope.halue = [];
+            console.log($scope.items);
             var obgJsonNew = JSON.stringify(boxs);
             localStorage.setItem("newLS", obgJsonNew);
         } else{
@@ -26,24 +29,24 @@ var boxs =  [
             pushName: "Ron",
             pushTime: "10:00",
             pushAge: 28,
-            pushOrg: "Second",
-            pushNote: ""
+            pushOrg: "Second"
+//            pushNote: ""
         },
         {
             pushData: "18/18/15",
             pushName: "Илья",
             pushTime: "10:45",
             pushAge: 22,
-            pushOrg: "First",
-            pushNote: ""
+            pushOrg: "First"
+//            pushNote: ""
         },
         {
             pushData: "18/18/15",
             pushName: "Michail",
             pushTime: "12:00",
             pushAge: 25,
-            pushOrg: "Last",
-            pushNote: ""
+            pushOrg: "Last"
+//            pushNote: ""
         }
     ];
 
---
+++ app/js/controllers/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $( "#datepicker" ).datepicker({
+        showOn: "button",
+        buttonImage: "images/calendar.gif",
+        buttonImageOnly: true,
+        buttonText: "Select date"
+    });
+
+//    $('.www').on('click', function(){
+//        $('.new_info input').attr('value', '');
+//    });
+
+
+});
